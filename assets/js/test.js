0dbdfcdfed8ba9e5fcabf59a0f399734aa453fb0
AJ <Pham>
2021-05-21 06:56:41 -0400

    robot combat update

---
+++ assets/js/test.js
@@ -0,0 +1,23 @@
+// var playerName = 'Clank McKrank';
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// fight function
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  
+};
+
+// run fight function to start game
+fight();
